d927ddebba43ee5a3893799cb2435191956d964a
Adam Mustapha <matawalle4u@gmail.com>
2020-07-28 16:37:00 -0700

    SQL Procedure Added

---
+++ sql/procedures.sql
@@ -0,0 +1,14 @@
+-- IDENTIFY ALL TABLES AND TABLES THEY AFFECT
+
+-- Sales -> Stocks|Request
+
+--- Procedures
+DELIMITER //
+
+  CREATE  PROCEDURE suna()
+    
+    BEGIN
+      -- code to be executed
+    END //
+
+DELIMETER ;
