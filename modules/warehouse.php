96250b123019240b4512a05086e9dfc476d089b2
Adam Mustapha <matawalle4u@gmail.com>
2020-08-06 22:16:09 -0700

    Fixed Racking System

--- modules/warehouse.php
+++ modules/warehouse.php
@@ -9,15 +9,9 @@
 
     private $warehouse_tbl = 'warehouses';
     private $zone_tbl = 'warehouse_zones';
-    private $racks_tbl = 'racks'
+    private $racks_tbl = 'racks';
 
-    private $tables = array(
-        'warehouses',
-        'warehouse_zones',
-        'racks'
-    );
-   
-
+    
     public function create_ware_house(array $columns, array $values){
 
         $create = $this->put($this->warehouse_tbl, $columns,$values);
@@ -31,6 +25,11 @@
 
     public function update_warehouse(array $columns, array $values, array $conds, array $conds_vals){
         $update = $this->update($this->warehouse_tbl, $columns, $values, $conds, $conds_vals);
+        if($update){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 
@@ -45,8 +44,13 @@
         }
     }
 
-    public function create_rack(){
-
+    public function create_rack(array $columns, array $values){
+        $create = $this->put($this->zone_tbl, $columns,$values);
+        if($create){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 
@@ -54,16 +58,19 @@
 
         $racks = $this->get($this->racks_tbl, $columns, $conditions, $values, $limit);
         return $racks;
+        
 
     }
 
     public function view_warehouse(array $columns, array $conditions, array $values, $limit){
-        $warehouses = $this->get($this->tables[0], $columns, $conditions, $values, $limit);
+
+        $warehouses = $this->get($this->warehouse_tbl, $columns, $conditions, $values, $limit);
         return $warehouses;
     }
 
-    public function view_warehouse_zone(){
-
+    public function view_warehouse_zone($columns, $conditions, $values, $limit){
+        $zones = $this->get($this->zone_tbl, $columns, $conditions, $values, $limit);
+        return $zones;
     }
 
 
@@ -76,14 +83,19 @@
 
    //$wh->update_warehouse(['name'], ["Updated Name 3333"], ['warehouse_id'],["3"]);
    //echo $wh->tables[0];
-   $www = $wh->view_warehouse(['name', 'address'], [], [], 'many');
-   echo $www[0]['name'];
-   echo "<br />". $www[1]['name'];
-   echo "<br />". $www[2]['name'];
+//    $www = $wh->view_warehouse(['name', 'address'], [], [], 'many');
+//    echo $www[0]['name'];
+//    echo "<br />". $www[1]['name'];
+//    echo "<br />". $www[2]['name'];
 
 //    $zon = $wh->create_warehouse_zone(['name', 'warehouse'], ["'Damage Zone'", "'3'"]);
 //    if($zon){
 //     echo 'sxxxx';
 //    }
+
+
+$z = $wh->view_warehouse_zone(['name', 'warehouse'], [], [], 'many');
+
+echo $z[0]['name'];
 
 ?>
