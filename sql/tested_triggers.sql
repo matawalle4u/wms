e4124197b8fdfd9c11c38d19a11495b9e205c915
Adam Mustapha <matawalle4u@gmail.com>
2020-08-19 04:31:18 -0700

    After Sales Made, Sales deleted, Damages Made Update Stock Trigger added

--- sql/tested_triggers.sql
+++ sql/tested_triggers.sql
@@ -10,6 +10,16 @@ CREATE TRIGGER `after_damages_insert`
   AFTER INSERT ON `damages` 
     FOR EACH ROW
       UPDATE stocks SET quantity=quantity-new.quantity WHERE product=new.product AND quantity>0
+
+CREATE TRIGGER `after_sales_insert` 
+  AFTER INSERT ON `sales` 
+    FOR EACH ROW
+      UPDATE stocks SET quantity=quantity-new.sold_qty WHERE product=new.product AND quantity>0 AND quantity-new.sold_qty>-1
+
+CREATE TRIGGER `after_sales_delete` 
+  AFTER DELETE ON `sales` 
+    FOR EACH ROW
+      UPDATE stocks SET quantity=quantity+old.sold_qty WHERE product=old.product
 --END OF WORKING TRIGGERS
 
 
