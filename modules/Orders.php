2bf023dfc5957d8dbfdf5a2d08859db2e97e2552
Adam <matawalle4u@gmail.com>
2020-08-28 17:25:54 -0700

    Order Update,Create checks availability

--- modules/Orders.php
+++ modules/Orders.php
@@ -7,7 +7,7 @@
         private $orders_tbl = 'orders';
 
         /*
-            $obj->update_order(['order_status'], ['Cancel'], ['order_id'], ['1']); working
+            update_order(['order_status','details'], ['Gwaji',"$deeetailss"], ['order_id'], ['1']); working
             1. Orders from shops (Problem comes when a customer)
             2. Orders from Websites
             3. Orders from B2B Platforms
@@ -25,12 +25,29 @@
             /*
                 $deeetailss = "products:rice;quantity:20;prices:20;selling_price:1;order_types:B2B;unit_measure:kg;product_id:5";
             */
+            $unavailables = $this->check_qty_details($values[1]);
+            if(empty($unavailables)){
+                echo 'make an roder';
+            }else{
+                echo'Some items are not available';
+            }
+
+            
+        }
+
+        public function get_order(array $columns, array $conditions, array $values, $limit){
+            $orders = $this->get($this->orders_tbl, $columns, $conditions, $values, $limit);
+            return $orders;
+        }
+
+
+        public function check_qty_details($details){
+
             $unwanted_chars  = array("'",",","."," ");
             $columns = array('customer','details','order_status');
             $unavailables = array();
-            
 
-            $details = $this->decompose($values[1]);
+            $details = $this->decompose($details);
             
             $products = $details[0];
             $quantities = $details[1];
@@ -40,73 +57,51 @@
             $unit = $details[5];
             $product_id = $details[6];
 
-
             foreach($products as $key2=>$value2){
 
                 $item_id = str_replace($unwanted_chars, "",$product_id[$key2]);
                 $item_qty = str_replace($unwanted_chars, "", $quantities[$key2]);
+                
                 $unit_mea = $unit[$key2];
                 $item_name = $products[$key2];
                 $order_ty = $order_type[$key2];
                 $price = $prices[$key2];
                 $selling_pri = $selling_price[$key2];
-                
-                
 
                 $produ = $this->get('stocks', ['quantity'], ['product'], [$item_id], 'single');
                 $line = "products:$item_name;quantity:$item_qty;prices:$price;selling_price:$selling_pri;order_types:$order_ty;unit_measure:$unit_mea;product_id:$item_id";
-
                 if(!empty($produ)){
-                    if($item_qty<$produ[0]['quantity']){
-                        $put  = $this->put($this->orders_tbl, $columns, $values);
-                        if($put==false){
-                            //Could not process Other return to user
-                        }
-                    }else{
-
-                        if($item_qty==$produ[0]['quantity']){
-                            $put = $this->put($this->orders_tbl, $columns, $values);
-                            if($put==false){
-                                //Could not process Other return to user
-                            }
-
-                        }else{
-                            
-                            array_push($unavailables, $line);
-                        }
-
-                        //TODO implement Request logic here
+                    if($item_qty>$produ[0]['quantity']){
+                        //Requesting too many 
+                        array_push($unavailables, $line);
                     }
-                    
                 }else{
                     array_push($unavailables, $line);
                 }
             }
-
             return $unavailables;
         }
 
-        public function get_order(array $columns, array $conditions, array $values, $limit){
-            $orders = $this->get($this->orders_tbl, $columns, $conditions, $values, $limit);
-            return $orders;
-        }
-
         public function update_order(array $columns, array $values, array $conds, array $conds_vals){
-            $update = $this->update($this->orders_tbl, $columns, $values, $conds, $conds_vals);
-            if($update){
-                return true;
+
+            $flag = 0;
+            if(in_array('details', $columns)){
+                $unav = $this->check_qty_details($values[array_search('details', $columns)]);
+                if(empty($unav)){
+                    $this->update($this->orders_tbl, $columns, $values, $conds, $conds_vals);
+                    $flag = 1;
+                }
             }else{
-                return false;
+                $this->update($this->orders_tbl, $columns, $values, $conds, $conds_vals);
+                $flag = 1;
             }
+            return $flag;
         }
 
 
         public function delete_order(array $conds, array $conds_vals){
             $this->delete($this->orders_tbl, $conds, $conds_vals);
         }
-
-
-
 
 
         public function decompose($text){
@@ -124,101 +119,20 @@
                         array_push($decomposed, $content);
                     }
                 }
-
             }
-
             return $decomposed;
         }
-
     }
 
-    
-    $obj = new Orders();
-    $customer = 1;
+    // $obj = new Orders();
+    // // $deeetailss = "products:rice, beans;quantity:200, 900;prices:20,90;selling_price:1,99;order_types:B2B,shop;unit_measure:kg, bag;product_id:5,5";
+    // // $un = $or->check_qty_details($deeetailss);
+    // // print_r($un);
+    // $customer =1;
+    // $deeetailss = "products:rice,beans;quantity:10,20;prices:20,25;selling_price:1,3;order_types:B2B,Shop;unit_measure:kg,bag;product_id:5,5";
+    // $values = ["'$customer'", "'$deeetailss'", "'Pending'"];
+    // $una = $obj->create_order($values);
+    // //echo $obj->update_order(['details', 'order_status'], ["$deeetailss", 'oee'], ['order_id'], ['1']);
 
-    $deeetailss = "products:rice;quantity:5000;prices:20;selling_price:1;order_types:B2B;unit_measure:kg;product_id:5";
-    $values = ["'$customer'", "'$deeetailss'", "'Pending'"];
-    $una = $obj->create_order($values);
-    if(empty($una)){
-        echo"Order Requested No any problem";
-    }else{
-        //print_r($una);
-        //Trying to process each returned lined
-        foreach($una as $lineKey=>$lineVal){
-            //echo"$lineVal";
-
-
-            $details = $obj->decompose($lineVal);
-
-           
-
-            //Products details array for every order
-            $products = $details[0];
-            $quantities = $details[1];
-            $prices = $details[2];
-            $selling_price = $details[3];
-            $order_type = $details[4];
-            $unit = $details[5];
-            $product_id = $details[6];
-
-            foreach($products as $key2=>$value2){
-
-                echo"<br />$value2 ($unit[$key2])
-                <input type=text name=$value2 value=$quantities[$key2]> <br />";
-                //echo 'ID '.$product_id[$key2]. ' ' .$value2 . ' Qty '. $quantities[$key2]. ' Unit '. $unit[$key2].' ';
-            }
-        
-        echo "<br />";
-
-
-
-
-        }
-    }
-
-
-
-    $orders = $obj->get_order(['customer', 'details', 'order_status'], ['order_id'], ['7'], 'many');
-
-    echo"
-        <form method=post action=>
-    ";
-
-    //Iterate every order for its details
-    foreach($orders as $key=>$value){
-
-        $details = $obj->decompose($orders[$key]['details']);
-
-        $customer  = $orders[$key]['customer'];
-        $order_status = $orders[$key]['order_status'];
-
-        //Products details array for every order
-        $products = $details[0];
-        $quantities = $details[1];
-        $prices = $details[2];
-        $selling_price = $details[3];
-        $order_type = $details[4];
-        $unit = $details[5];
-        $product_id = $details[6];
-
-        foreach($products as $key2=>$value2){
-
-            echo"$value2 ($unit[$key2])
-            <input type=text name=$value2 value=$quantities[$key2]> <br />";
-            //echo 'ID '.$product_id[$key2]. ' ' .$value2 . ' Qty '. $quantities[$key2]. ' Unit '. $unit[$key2].' ';
-        }
-        
-        echo "<br />";
-
-
-    }
-
-    echo"
-
-    <input type=submit value=Update name=order_updated>
-    
-    </form>";
-
-    
 
 ?>
