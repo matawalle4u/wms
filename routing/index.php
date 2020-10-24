dda26e1aca49fa51c2cbcdad25314696b8f2098d
Adam <matawalle4u@gmail.com>
2020-10-24 03:14:51 -0700

    Restored

--- routing/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include('route.php');
-
-    $route = new Route();
-    $route->add("/about");
-    print_r($route);
-    $route->submit();
-?>
--- sql/barcode/sql_file.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `barcode` (
-  `product_id` int(11) NOT NULL AUTO_INCREMENT,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
